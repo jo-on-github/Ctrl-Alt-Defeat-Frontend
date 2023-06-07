35f814c4ea0cae0af7c8cce008f01cb05b2b9c7d
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-07 16:34:26 +0100

    React app created and boilerplate for homepage done

---
+++ frontend/src/components/Homepage/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Homepage() {
+    return (
+        <div className="overlay">
+            <div className="header">
+                <div className="header__btn--back">
+                </div>
+                <div className="header__marker">
+                </div>
+                <div className="header__title">  
+                </div>
+                <div className="header__btn--profile">
+                </div>
+            </div>
+            <div className="search">
+                <div className="search__input">
+                </div>
+                <div className="search__btn--filter">
+                </div>
+            </div>
+            <div className="main">
+                <div className="main__list">
+                </div>
+            </div>
+            <div className="footer">
+            </div>
+        </div>
+    );
+}
