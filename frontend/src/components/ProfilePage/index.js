f7cca4fe7922b55d9561a86f2afa60d988d599c8
Jo <jo.chattoo@gmail.com>
2023-06-08 15:53:28 +0100

    profile boilerplate complete

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -4,15 +4,42 @@ function ProfilePage() {
   return (
     <div className="overlay">
       <div className="header">
-        <div className="header__btn--back"></div>
+        <div className="header__btn--back">
 
-        <div className="header__btn--addGuide"></div>
+        </div>
+
+        <div className="header__btn--addGuide">
+
+        </div>
 
         <div className="profile">
-          <div className="profile__title--profileName"></div>
+          <div className="profile__title--profileName">
 
-          <div className="profilePicture"></div>
+          </div>
+
+          <div className="profile__img--profileImg">
+
+          </div>
         </div>
+
+        <div className="profile__navBar">
+          <div className="profile__navBar--personalInfo">
+
+          </div>
+          <div className="profile__navBar--interests">
+
+          </div>
+          <div className="profile__navBar--guides">
+
+          </div>
+        </div>
+
+        <div className="profile__content">
+
+        </div>
+      </div>
+      <div className="footer">
+        
       </div>
     </div>
   );
