3d97c05c654a9ec8f093d79f652316f1d8178671
Danayet Gezaie <124500874+Danayet@users.noreply.github.com>
2023-06-08 15:35:32 +0100

    profile name and header added

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,28 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function ProfilePage() {
-    return (
-        <div className="overlay">
-       
-        <div className="header">
-     
-            <div className="header__btn--back">
-            </div>
-            <div className="header__btn--addGuide">
-            <div/>
-<div className ="profile">
-            <div className="header__profileName">  
-            </div>
-            <div className="header__profilePicture">
-            </div>
-            </div>
-            <div className="header__btn--profile">
-            </div>
-            </div>
-            </div>
-     
-        <div className="profileImage">
-        </div>
-        </div>
+  return (
+    <div className="overlay">
+      <div className="header">
+        <div className="header__btn--back"></div>
 
-    )}
+        <div className="header__btn--addGuide"></div>
+
+        <div className="profile">
+          <div className="profile__title--profileName"></div>
+
+          <div className="profilePicture"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
