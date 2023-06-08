62bd91020c91e2988761500f21c177d067c9bc64
Shervin <shervinmohajeri@gmail.com>
2023-06-08 15:57:41 +0100

    nav bar boilerplate complete

--- frontend/src/components/CreateItinerary/index.js
+++ frontend/src/components/CreateItinerary/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function CreateItinerary() {
   return (
     <div className="overlay">
-       <div className="header">
-            <div className="header__btn--back">
-            </div>
-            <div className="header__marker">
-            </div>
-            <div className="header__title">  
-            </div>
-            <div className="header__btn--profile">
-            </div>
+      <div className="header">
+        <div className="header__btn--back">
         </div>
-        <div className="userInput">
-            <div className="userInput__input">
-            </div>
-            <div className="userInput__btn--submit">
-            </div>
+        <div className="header__marker">
         </div>
-        <div className="footer">
+        <div className="header__title">
         </div>
+        <div className="header__btn--profile">
+        </div>
+      </div>
+      <div className="userInput">
+        <div className="userInput__input">
+        </div>
+        <div className="userInput__btn--submit">
+        </div>
+      </div>
+      <div className="footer">
+      </div>
     </div>
   );
 }
