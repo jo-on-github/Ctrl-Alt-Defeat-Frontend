7cd3ca5f0ca72a33dc5850d4f51768bd3ae24cae
Jo <jo.chattoo@gmail.com>
2023-06-08 14:30:33 +0100

    header & profile done

---
+++ frontend/src/components/ProfilePage/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function ProfilePage() {
+    return (
+        <div className="overlay">
+       
+        <div className="header">
+     
+            <div className="header__btn--back">
+            </div>
+            <div className="header__btn--addGuide">
+            <div/>
+<div className ="profile">
+            <div className="header__profileName">  
+            </div>
+            <div className="header__profilePicture">
+            </div>
+            </div>
+            <div className="header__btn--profile">
+            </div>
+            </div>
+            </div>
+     
+        <div className="profileImage">
+        </div>
+        </div>
+
+    )}
