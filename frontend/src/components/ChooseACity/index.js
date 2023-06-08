e4ec59672a8b42bd45e52c259ffd3216dfec39cc
Shervin <shervinmohajeri@gmail.com>
2023-06-08 14:21:39 +0100

    choose a city page boilerplate done

---
+++ frontend/src/components/ChooseACity/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function ChooseACity() {
+    return (
+        <div className="overlay">
+            <div className="header">
+                <div className="header__title">
+                </div>
+                <div className="header__img--logo">
+                </div>
+            </div>
+            <div className="userInput">
+                <div className="userInput__input">
+                </div>
+                <div className="userInput__btn--randomiser">
+                </div>
+            </div>
+            <div className="main">
+                <div className="main__animated--globe">           
+                </div>
+                <div className="main__btn--submit">
+                </div>
+            </div>
+
+            
+        </div>
+    )
+}
