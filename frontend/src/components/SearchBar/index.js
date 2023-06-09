6e6037a084f2a3ea6b735a7ba0d2956e4cb1302b
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-09 16:36:14 +0100

    Started on search component styling

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import {Button} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import TuneIcon from '@mui/icons-material/Tune';
 import "./styles.css";
 
 function SearchBar() {
     return (
         <div className="search">
-            <div className="search__searchbox">Search bar here</div>
-            <div className="search__filter">filter button</div>
+            <div className="search__searchbox">
+            <input type="text" placeholder="Search..."></input>
+            </div>
+            <div className="search__filter">
+                <Button>
+                    <TuneIcon />
+                </Button>
+            </div>
         </div>
     );
 }
