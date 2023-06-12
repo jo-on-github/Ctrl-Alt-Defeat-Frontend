e77b8e78c51c6e0528e01711cbc4d2f372dfe193
Danayet Gezaie <124500874+Danayet@users.noreply.github.com>
2023-06-12 12:04:52 +0100

    ChooseACity css started

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import Favourites from "../Favourites";
+import ChooseACity from "../ChooseACity";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <ChooseACity />
+  </div>;
 }
 
 export default App;
