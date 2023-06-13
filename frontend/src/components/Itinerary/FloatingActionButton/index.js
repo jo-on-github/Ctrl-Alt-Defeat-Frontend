68c66a36f00a146840ffc535e0da98a145caa343
danben1 <124446045+danben1@users.noreply.github.com>
2023-06-13 15:55:56 +0100

    Added responsive features - media queries for selective rendering, sticky top and bottom nav bar, adjusted breakpoints on itinerary list item grid

---
+++ frontend/src/components/Itinerary/FloatingActionButton/index.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+
+export default function FloatingActionButton() {
+    return (
+        <Box
+            sx={{
+                "& > :not(style)": { m: 1 },
+                position: "fixed",
+                top: 70,
+                right: 10,
+            }}
+        >
+            <Fab
+                variant="extended"
+                size="small"
+                color="primary"
+                aria-label="add"
+            >
+                <AddIcon sx={{ mr: 1 }} />
+                New trip
+            </Fab>
+        </Box>
+    );
+}
