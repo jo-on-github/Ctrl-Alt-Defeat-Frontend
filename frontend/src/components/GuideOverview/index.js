11f56498a6725d12b97b0c3a7a35197752c42a5d
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-13 16:28:05 +0100

    guide overview heading, subheading and img complete

--- frontend/src/components/GuideOverview/index.js
+++ frontend/src/components/GuideOverview/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./styles.css";
 import { Button } from "@mui/material";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ThumbDownAltOutlinedIcon from "@mui/icons-material/ThumbDownAltOutlined";
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import profileImg from "../../assets/profileImg.png";
+import cath from "../../assets/cath.png";
 
 function GuideOverview() {
   const buttonStyle = {
@@ -31,10 +36,28 @@ function GuideOverview() {
       </div>
 
       <div className="guideSubHeader">
-        <div className="guideSubHeader__icon--thumbUp"></div>
-        <div className="guideSubHeader__icon--thumbDown"></div>
-        <div className="guideSubHeader__icon--pin"></div>
-        <div className="guideSubHeader__icon--favourite"></div>
+        <div className="guideSubHeader__icons">
+          <div className="guideSubHeader__icons--thumbUp">
+            <ThumbUpAltOutlinedIcon />
+            <span className="guideSubHeader__icons--thumbUpRating">88%</span>
+          </div>
+          <div className="guideSubHeader__icons--thumbDown">
+            <ThumbDownAltOutlinedIcon />
+            <span className="guideSubHeader__icons--thumbDownRating">12%</span>
+          </div>
+        </div>
+        <div className="guideSubHeader__additionalIcons">
+          
+          <div className="guideSubHeader__additionalIcons--pin">
+            <PushPinOutlinedIcon />
+          </div>
+          <div div className="guideSubHeader__additionalIcons--favourite">
+            <BookmarkBorderOutlinedIcon />
+          </div>
+        </div>
+      </div>
+      <div className="guide__img">
+        <img src={cath} alt="cath" />
       </div>
     </div>
   );
