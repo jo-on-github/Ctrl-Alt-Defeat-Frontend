5e2a46136b84bc7fcdcae9b17bea5355edd48825
Shervin <shervinmohajeri@gmail.com>
2023-06-14 15:17:44 +0100

    updated version of app

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -10,21 +10,33 @@ import Homepage from "../Homepage";
 import Itinerary from "../Itinerary";
 
 //import ProfilePage from "../ProfilePage";
-import ChooseACity from "../ChooseACity";
+// import ChooseACity from "../ChooseACity";
 //import Favourites from "../Favourites";
 //import ListItem from "../ListItem";
 //import GuideOverview from "../GuideOverview";
 // import Itinerary from "../Itinerary";
 
+
+
 import ChooseACity from "../ChooseACity";
 
 
+
+
 function App() {
-  
+  //Defining Media Queries for responsive design -
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+});
+const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+//const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+//const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
+
+//Logic inside return statement either renders app or renders error message dependent on size of screen vieweing content.
+//If you need to render something to screen render it after {isTabletorMobile && RENDER-COMPONENT-HERE}
     return (
         <div>
-
-            <ChooseACity />
 
             {isDesktopOrLaptop && (
                 <p>
