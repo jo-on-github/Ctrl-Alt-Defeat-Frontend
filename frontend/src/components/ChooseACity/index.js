227056f81b771036f453721ac05143dc182ca5b9
danben1 <124446045+danben1@users.noreply.github.com>
2023-06-14 16:04:57 +0100

    added route from landing to homepage

--- frontend/src/components/ChooseACity/index.js
+++ frontend/src/components/ChooseACity/index.js
@@ -1,35 +1,44 @@
-
-import React from 'react';
-import './styles.css';
-import SearchIcon from '@mui/icons-material/Search';
-
-
+import React from "react";
+import "./styles.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { NavLink } from "react-router-dom";
 
 function ChooseACity() {
-  return (
-    <div className="overlay">
-      <div className="header">
-        <div className="header__title">
-          <h1>Neighbourhood Nomad</h1>
-        </div>
-        <div className="header__img--logo">
-          <img className="logo__image" src="../image/image-(2).png" alt="logo" />
-        </div>
-      </div>
+    return (
+        <div className="overlay">
+            <div className="header">
+                <div className="header__title">
+                    <h1>Neighbourhood Nomad</h1>
+                </div>
+                <div className="header__img--logo">
+                    <img
+                        className="logo__image"
+                        src="../image/image-(2).png"
+                        alt="logo"
+                    />
+                </div>
+            </div>
 
-      <div className="userInput">
-        <input className="userInput__input" type="text" placeholder="Choose your location..." />
-        <SearchIcon className="userInput__icon" />
-        <button className="userInput__btn--randomiser">I'm feeling adventurous!</button>
-      </div>
+            <div className="userInput">
+                <input
+                    className="userInput__input"
+                    type="text"
+                    placeholder="Choose your location..."
+                />
+                <SearchIcon className="userInput__icon" />
+                <NavLink className="navlink" to={"home"}>
+                    <button className="userInput__btn--randomiser">
+                        I'm feeling adventurous!
+                    </button>
+                </NavLink>
+            </div>
 
-        <div className="main">
-        <div className="main__animated--globe">globe</div>
-        <button className="main__btn--submit">EXPLORE CITY</button>
-      </div>
-    </div>
-  )
+            <div className="main">
+                <div className="main__animated--globe">globe</div>
+                <button className="main__btn--submit">EXPLORE CITY</button>
+            </div>
+        </div>
+    );
 }
-
 
 export default ChooseACity;
