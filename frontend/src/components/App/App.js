1047bf83ca904060b1e8ac6e60c11981e42e1d4c
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-15 15:24:56 +0100

    app code cleaned

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,34 +1,16 @@
 import "./App.css";
 import React from "react";
-
-// import { useMediaQuery } from "react-responsive";
-//import Homepage from "../Homepage";
-
 import { useMediaQuery } from "react-responsive";
-
 import Homepage from "../Homepage";
 import Itinerary from "../Itinerary";
 import {Route, Routes} from "react-router-dom";
-
-//import ProfilePage from "../ProfilePage";
-// import ChooseACity from "../ChooseACity";
-//import Favourites from "../Favourites";
-//import ListItem from "../ListItem";
 import GuideOverview from "../GuideOverview";
-// import Itinerary from "../Itinerary";
-
-
-
 import ChooseACity from "../ChooseACity";
 
 
 
 
 function App() {
-  //Defining Media Queries for responsive design -
-  const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 1224px)",
-});
 const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
 const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
