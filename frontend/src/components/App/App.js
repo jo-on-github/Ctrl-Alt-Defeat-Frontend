faa60ec6b383c1bc5f181376785567d4c8501ef3
unknown <shamy_pw4@hotmail.co.uk>
2023-06-16 11:06:32 +0100

    Fixed navlinks and css structure on Guide Overview page

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -6,6 +6,8 @@ import { Route, Routes} from "react-router-dom";
 import GuideOverview from "../GuideOverview";
 import ChooseACity from "../ChooseACity";
 import Overview from "../GuideOverview/overview/index.js";
+import Experience from "../GuideOverview/experience/index.js";
+import Reviews from "../GuideOverview/reviews/index.js";
 
 function App() {
   const [city, setCity] = React.useState("hi");
@@ -22,8 +24,8 @@ function App() {
         <Route path="/planner" element={<Itinerary />} />
         <Route path="/guide" element={<GuideOverview />}>
           <Route path="/guide/overview" element={<Overview />} />
-          <Route path="/guide/experience" element={<h1>Experience</h1>} />
-          <Route path="/guide/reviews" element={<h1>Reviews</h1>} />
+          <Route path="/guide/experience" element={<Experience />} />
+          <Route path="/guide/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </div>
