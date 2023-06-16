98bb454d8df2345eb93b06b139fbcedc5e3b2950
Jo <jo.chattoo@gmail.com>
2023-06-16 11:06:16 +0100

    desktop responsiveness complete... yaaay! Still need to fix search icon

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -7,12 +7,13 @@ import dummy from "../images/profile_photo/dummy.jpg";
 import ListItem from "../ListItem";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import useMediaQueries from "media-queries-in-react";
 import useMediaQuery from '@mui/material/useMediaQuery'
 
 
+
 function Homepage() {
-  const matches = useMediaQuery('(min-width:833px)');
+  const matches = useMediaQuery('(min-width: 833px)');
+  const matches2 = useMediaQuery('(min-width: 1279px)');
 
   return (
     <div className="overlay">
@@ -25,39 +26,40 @@ function Homepage() {
       <div className="main">
         <div className="main__list">
           <Box className="main__listItems" sx={{ flexGrow: 1 }}>
-            <Grid
-              className="main__listItems--grid"
-              container
-              spacing={2}
-            >
-              <Grid item xs={6} sm={4} md={3}>
+            <Grid className="main__listItems--grid" container spacing={2}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
-              <Grid item xs={6} sm={4} md={3}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
-              <Grid item xs={6} sm={4} md={3}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
-              <Grid item xs={6} sm={4} md={3}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
-              <Grid item xs={6} sm={4} md={3}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
-              <Grid item xs={6} sm={4} md={3}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
-              <Grid item xs={6} sm={4} md={3}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
-              <Grid item xs={6} sm={4} md={3}>
+              <Grid item xs={12} sm={6} md={4}>
                 <ListItem />
               </Grid>
 
               {matches && (
-                <Grid item xs={6} sm={4} md={3}>
+                <Grid item xs={12} sm={6} md={4}>
                   <ListItem />
+                </Grid>
+              )}
+
+              {matches2 && (
+                <Grid item xs={12} sm={6} md={4}>
                 </Grid>
               )}
             </Grid>
