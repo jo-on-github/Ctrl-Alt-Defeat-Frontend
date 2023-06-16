db80b2125f75f36e78a1116e33bdf1aeb336cd9a
danben1 <124446045+danben1@users.noreply.github.com>
2023-06-16 13:24:17 +0100

    updated grid on homepage, revamped search bar

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -9,66 +9,57 @@ import ListItem from "../ListItem";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
-import useMediaQuery from '@mui/material/useMediaQuery'
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { Link } from "react-router-dom";
 
 function Homepage({ city, updateSearchFilter }) {
-  const [searchFilter, setSearchFilter] = React.useState(null);
-  const matches = useMediaQuery('(min-width: 833px)');
-  const matches2 = useMediaQuery('(min-width: 1279px) ')
-  const handleSearchFilterChange = (searchFilter) => {
-    setSearchFilter(searchFilter);
-  };
+    const [searchFilter, setSearchFilter] = React.useState(null);
+    const matches = useMediaQuery("(min-width: 833px)");
+    const matches2 = useMediaQuery("(min-width: 1279px) ");
 
+    const handleSearchFilterChange = (searchFilter) => {
+        setSearchFilter(searchFilter);
+    };
 
-  return (
-    <div className="overlay">
-      <div className="header">
-
-        <Header imageUrl={dummy} altText="My Image" location={city} />
-      </div>
-      <div className="search">
-        <SearchBar
-          handleSearchFilterChange={handleSearchFilterChange}
-          updateSearchFilter={updateSearchFilter}
-        />
-      </div>
-      <div className="main">
-        <div className="main__list">
-          <Box className="main__listItems" sx={{ flexGrow: 1 }}>
-            <Grid className="main__listItems--grid" container spacing={2}>
-
-
-              {listItemData.map((item, index) => (
-                <Grid item xs={6} sm={4} md={3} key={index}>
-                  <Link to="/guide/overview">
-                    <ListItem
-                      title={item.title}
-                    />
-                  </Link>
+    return (
+        <div className="overlay">
+            <Header
+                sx={{ zIndex: 1 }}
+                imageUrl={dummy}
+                altText="My Image"
+                location={city}
+            />
+            <div className="search">
+                <SearchBar
+                    sx={{ zIndex: 0 }}
+                    handleSearchFilterChange={handleSearchFilterChange}
+                    updateSearchFilter={updateSearchFilter}
+                />
+            </div>
+            <Box className="main__listItems" sx={{ flexGrow: 1 }}>
+                <Grid className="main__listItems--grid" container spacing={2}>
+                    {listItemData.map((item, index) => (
+                        <Grid item xs={6} sm={4} md={3} key={index}>
+                            <Link to="/guide/overview">
+                                <ListItem title={item.title} />
+                            </Link>
+                        </Grid>
+                    ))}
+                    {matches && (
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ListItem />
+                        </Grid>
+                    )}
+                    {matches2 && (
+                        <Grid item xs={12} sm={6} md={4}>
+                            {/* Add the contents for matches2 here */}
+                        </Grid>
+                    )}
                 </Grid>
-
-      {matches && (
-                <Grid item xs={12} sm={6} md={4}>
-                  <ListItem />
-                </Grid>
-              )}
-
-              {matches2 && (
-                <Grid item xs={12} sm={6} md={4}>
-        
-              ))}
-
-            </Grid>
-          </Box>
+            </Box>
+            <NavBar />
         </div>
-      </div>
-      <div className="footer">
-        <NavBar />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Homepage;
-
--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,35 +1,69 @@
-import React from "react";
-import { Button } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+// import React from "react";
+// import { Button } from "@mui/material";
+// import SearchIcon from "@mui/icons-material/Search";
+// import TuneIcon from "@mui/icons-material/Tune";
+// import "./styles.css";
+
+// function SearchBar({ updateSearchFilter }) {
+
+//     function handleChange(event) {
+//         updateSearchFilter(event.target.value);
+//     }
+
+//   return (
+//     <div className="search">
+//       <div className="search__searchbox">
+//         <span className="search_searchbox--searchIcon">
+//           <SearchIcon />
+//         </span>
+//         <input
+//           type="text"
+//           placeholder="Search..."
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div className="search__filter">
+//         <Button>
+//           <TuneIcon />
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SearchBar;
+
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
 import TuneIcon from "@mui/icons-material/Tune";
-import "./styles.css";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
 
-function SearchBar({ updateSearchFilter }) {
-  
-
-    function handleChange(event) {
-        updateSearchFilter(event.target.value);
-    }
-
-  return (
-    <div className="search">
-      <div className="search__searchbox">
-        <span className="search_searchbox--searchIcon">
-          <SearchIcon />
-        </span>
-        <input
-          type="text"
-          placeholder="Search..."
-          onChange={handleChange}
-        />
-      </div>
-      <div className="search__filter">
-        <Button>
-          <TuneIcon />
-        </Button>
-      </div>
-    </div>
-  );
+export default function SearchBar() {
+    return (
+        <Paper
+            component="form"
+            sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: 400,
+            }}
+        >
+            <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Choose a guide"
+                inputProps={{ "aria-label": "search google maps" }}
+            />
+            <IconButton sx={{ p: "10px" }} aria-label="menu">
+                <TuneIcon />
+            </IconButton>
+        </Paper>
+    );
 }
-
-export default SearchBar;
