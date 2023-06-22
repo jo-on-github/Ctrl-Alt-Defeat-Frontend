58f8b886a94d17acdd8bf27a3af23a3cc0ba3e98
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-22 15:55:52 +0100

    basic validation for log in page

--- frontend/src/components/LogIn/index.js
+++ frontend/src/components/LogIn/index.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
 
 
+
+
 function LogInSignUp() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    if ((email === "") || (password === ""))   {
+      setError(true);
+      return;
+    }
+    
+  }
+
+  const handleErrorClick = (e) => {
+    setError(false);
+  }
+
+
     return(
        <div className="login_overlay">
+        {error && 
+            <div className="modal-overlay">
+                <div 
+                    className="error"><p>Require both Email and Password fields. Please try again.</p>
+                    <button onClick={handleErrorClick}>Okay!</button>
+                </div>
+            </div>}
             <div className="header">
                 <h1 className="header__title">Welcome To Neighbourhood Nomad</h1>
             </div>
@@ -18,9 +54,20 @@ function LogInSignUp() {
                     <div className="login_container__form">
                         
                         <form>
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
-                            <button type="submit">Log In</button>
+                            <input 
+                                type="email" 
+                                placeholder="Email" 
+                                value={email}
+                                 onChange={handleEmailChange}
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Password"
+                                value={password}
+                                onChange={handlePasswordChange}
+                                
+                             />
+                            <button type="submit" onClick={handleSubmit}>Log In</button>
 
                         </form>
                         <div className="login_container__signup_container">
