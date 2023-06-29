4fd55c6db3f73ec7f55e86939b837055f38d0b61
danben1 <124446045+danben1@users.noreply.github.com>
2023-06-29 11:41:04 +0100

    added subheadings to overview component

--- frontend/src/components/overview/index.js
+++ frontend/src/components/overview/index.js
@@ -3,30 +3,35 @@ import "./styles.css";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 
+export default function Overview({ chosenCity, setChosenCity }) {
+    useEffect(() => {
+        const storedCity = localStorage.getItem("chosenCity");
+        if (storedCity) {
+            setChosenCity(JSON.parse(storedCity));
+        }
+    }, []);
 
+    const { id } = useParams();
 
-export default function Overview({chosenCity, setChosenCity}) {
-
-  useEffect(() => {
-    const storedCity = localStorage.getItem("chosenCity");
-    if (storedCity) {
-      setChosenCity(JSON.parse(storedCity));
-    }
-  }, []);
-  
-  const {id} = useParams();
-
-  const selectedItem = chosenCity.find(item => item._id === id);
-  console.log(chosenCity);
-  return (
-    <div className="subInfo">
-      <div className="subInfo__title">Overview</div>
-      <div className="subInfo__description">
-       {selectedItem.overview}
-       <p>{selectedItem.highlights}</p>
-      </div>
-
-  
-    </div>
-  );
+    const selectedItem = chosenCity.find((item) => item._id === id);
+    console.log(chosenCity);
+    return (
+        <div className="subInfo">
+            <div className="subInfo__title">Overview</div>
+            <div className="subInfo__description">
+                Summary: <br />
+                {selectedItem.overview} <br />
+                <br />
+                Budget: <br />
+                {selectedItem.budget} <br />
+                <br />
+                <ul>
+                    {selectedItem.highlights.map((item) => {
+                        return <li>{item}</li>;
+                    })}
+                </ul>
+                ;
+            </div>
+        </div>
+    );
 }
